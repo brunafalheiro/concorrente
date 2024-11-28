06dcaf6e3ffaafa01b66446b756529b962e101af
Júlio Burlamaqui <64053910+JulioBurlamaqui@users.noreply.github.com>
2024-11-28 18:25:24 -0300

    fix bash
    Agora testa com 4 e 8 threads simultaneamente

--- final/script.sh
+++ final/script.sh
@@ -7,7 +7,8 @@ gcc -o sequencial sequencial.c
 # Parâmetros
 NODES=500
 PROBABILIDADE=95
-NTHREADS=8
+NTHREADS_TEST1=4
+NTHREADS_TEST2=8
 
 MATRIZ_FILE="matriz${NODES}"
 SEQ_FILE="seq${NODES}"
@@ -16,7 +17,8 @@ CONC_FILE="conc${NODES}"
 # Executa os programas
 ./gera_grafo $NODES $PROBABILIDADE $MATRIZ_FILE
 ./sequencial $MATRIZ_FILE $SEQ_FILE
-./concorrente $MATRIZ_FILE $CONC_FILE $NTHREADS
+./concorrente $MATRIZ_FILE $CONC_FILE $NTHREADS_TEST1
+./concorrente $MATRIZ_FILE $CONC_FILE $NTHREADS_TEST2
 
 # Compara os resultados
 if diff $SEQ_FILE $CONC_FILE > /dev/null; then
